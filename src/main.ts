edbdcd55d19506a9a7b2ea63e2165610df525b62
Sergey Kosykh <s3rkosal@gmail.com>
2026-02-06 08:22:46 +0400

    little refactor

--- src/main.ts
+++ src/main.ts
@@ -1,127 +1,90 @@
 import './style.css'
 
-const canv = document.querySelector<HTMLCanvasElement>('#webgl-canvas');
+import vertShaderSource from './shaders/default.vert?raw';
+import fragShaderSource from './shaders/default.frag?raw';
 
-const vertShaderSource =
-`#version 300 es
-     
-in vec4 a_position;
-     
-void main() {
-  gl_Position = a_position;
-}`;
+import './shaders.ts';
+import createShaders from './shaders.ts';
 
-const fragShaderSource =
-`#version 300 es
+const positions = [
+    -1.0, -1.0,
+     1.0,  1.0,
+    -1.0,  1.0
+]
 
-precision highp float;
-     
-out vec4 outColor;
- 
-void main() {
-  outColor = vec4(1, 0, 0.5, 1);
-}`;
-
-function createShader(cntx: WebGL2RenderingContext, type: GLenum, source: string) {
-  let shader = cntx.createShader(type);
-
-  if (!shader)
-    return;
-
-  cntx.shaderSource(shader, source);
-  cntx.compileShader(shader);
-
-  let success = cntx.getShaderParameter(shader, cntx.COMPILE_STATUS);
-
-  if (success)
-    return shader;
-}
-
-function linkShaders(cntx: WebGL2RenderingContext, vertShader: WebGLShader, fragShader: WebGLShader) {
-  const program = cntx.createProgram();
-  cntx.attachShader(program, vertShader);
-  cntx.attachShader(program, fragShader);
-  cntx.linkProgram(program);
-
-  const success = cntx.getProgramParameter(program, cntx.LINK_STATUS);
-  if (success)
-    return program;
-
-  console.log(cntx.getProgramInfoLog(program));
-  cntx.deleteProgram(program);
-}
-
-
-function main() {
-  if (!canv || !canv.getContext) {
+function init() : {
+  cntx: WebGL2RenderingContext,
+  program: WebGLProgram,
+  vao: WebGLVertexArrayObject
+} | undefined {
+  // check html canvas and WebGL2 context
+  const canv = document.querySelector<HTMLCanvasElement>('#webgl-canvas');
+  if (!canv) {
     console.log("Could not initialize canvas element!");
     return;
   }
-  
-  let cntx = canv?.getContext("webgl2");
-
+  const cntx = canv?.getContext("webgl2");
   if (!cntx) {
     console.log('WebGL 2 is not supported on this system!');
     return;
   }
 
-  // shaders creation
-  const vertShader = createShader(cntx, cntx.VERTEX_SHADER, vertShaderSource);
-  const fragShader = createShader(cntx, cntx.FRAGMENT_SHADER, fragShaderSource);
-
-  if (!vertShader) {
-    console.log('Could not compile vertex shader!');
-    return;
-  }
-  if (!fragShader) {
-    console.log('Could not compile fragment shader!');
-    return;
-  }
-
-  const program = linkShaders(cntx, vertShader, fragShader);
-
+  // shader program
+  const program = createShaders(cntx, vertShaderSource, fragShaderSource);
   if (!program) {
     console.log('Could not compile shaders into the program!');
     return;
   }
   
-  const posAttribLoc = cntx.getAttribLocation(program, "a_position");
-
-  
-    // position buffer 
+  // position buffer 
   const posBuffer = cntx.createBuffer();
   cntx.bindBuffer(cntx.ARRAY_BUFFER, posBuffer);
-  const positions = [
-    -1.0, -1.0,
-     1.0,  1.0,
-    -1.0,  1.0
-  ]
   cntx.bufferData(cntx.ARRAY_BUFFER, new Float32Array(positions), cntx.STATIC_DRAW);
 
+  // vertex array
   const vao = cntx.createVertexArray();
   cntx.bindVertexArray(vao);
-  cntx.enableVertexAttribArray(posAttribLoc);
-
-  const sz     = 2;
-  const type   = cntx.FLOAT;
-  const norm   = false;
-  const stride = 0;
-  const vap_offset = 0;
+  
+  // vertex shader input attrib
+  const posAttribLoc = cntx.getAttribLocation(program, "a_position");
+  cntx.enableVertexAttribArray(posAttribLoc);  
+  const sz     = 2; const type   = cntx.FLOAT; const norm   = false;
+  const stride = 0; const vap_offset = 0;
   cntx.vertexAttribPointer(posAttribLoc, sz, type, norm, stride, vap_offset);
 
+  // viewport
   cntx.viewport(0, 0, cntx.canvas.width, cntx.canvas.height);
 
+  return {cntx, program, vao};
+}
+
+function draw(
+  cntx: WebGL2RenderingContext,
+  program: WebGLProgram,
+  vao: WebGLVertexArrayObject,
+  primitive = cntx.TRIANGLES,
+  offset = 0,
+  num = 3,
+) {
+
+  // clearing 
   cntx.clearColor(0, 0, 0, 0);
   cntx.clear(cntx.COLOR_BUFFER_BIT);
   cntx.useProgram(program);
 
+  // binding verticies
   cntx.bindVertexArray(vao);
-  const primitiveType = cntx.TRIANGLES;
-  const offset = 0;
-  const count = 3;
 
-  cntx.drawArrays(primitiveType, offset, count); 
-  
+  // draw call
+  cntx.drawArrays(primitive, offset, num); 
+}
+
+function main() {
+  const init_res = init();
+  if (init_res) {
+    const {cntx, program, vao} = init_res;
+    draw(cntx, program, vao);
+  }
 }
 
 main();
