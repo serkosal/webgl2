616f02b3bc3774ec422ed9b55483087ab77c4e32
Sergey Kosykh <s3rkosal@gmail.com>
2026-02-15 01:04:58 +0400

    fixed gh-page build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(
-    {  
+    {
+        base: '/webgl2/',
         plugins: [
             tailwindcss(),
         ],
